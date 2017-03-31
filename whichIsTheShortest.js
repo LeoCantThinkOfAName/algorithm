a6002af17836d955458cfe7101220c35c634097f
unknown <i7851@hotmail.com>
2017-03-31 17:36:40 +0800

    modify output message

--- whichIsTheShortest.js
+++ whichIsTheShortest.js
@@ -12,30 +12,28 @@ let A = [3, 3],
     I = [9, 4],
     J = [5, 9];
 
-function findTheShortest(num, ...array) {
-    //merge all array in one
-    let arr = [].concat(...array);
-    
+function findTheShortest(num, ...arrays) {
     //init empty arry
     let arrX = [],
         arrY = [],
         result = [];
 
-    //seperate X and Y into two array
-    for(let i = 0; i < arr.length; i ++) {
-        i % 2 === 0 ? arrX.push(arr[i]) : arrY.push(arr[i]);
+    //seperate X and Y to two array
+    for(let i = 0; i < arrays.length; i ++) {
+        arrX.push(arrays[i][0]);
+        arrY.push(arrays[i][1]);
     }
 
     for(let i = 0; i < arrX.length; i ++) {
         for(let j = i + 1; j < arrX.length; j ++) {
             //setup obj key for better readability
-            let points = String.fromCharCode(65 + i) + '-' + String.fromCharCode(65 + j);
+            let pos = String.fromCharCode(65 + i) + '-' + String.fromCharCode(65 + j);
 
             //calculate distances between each point
             let dis = Math.sqrt(Math.pow(arrX[i] - arrX[j], 2) + Math.pow(arrY[i] - arrY[j], 2));
 
             //create object and push it into array
-            let obj = {points: points, distance: dis};
+            let obj = {points: pos, distance: dis};
             result.push(obj);
         }
     }
@@ -44,6 +42,6 @@ function findTheShortest(num, ...array) {
     result.sort(function(x, y) { return (x.distance > y.distance ? 1 : y.distance > x.distance ? -1 : 0)});
     
     console.log(result);
-    console.log('The shortest distance between all the points is ' + result[num].points + ' : ' +  result[num].distance);
+    console.log('The Shortest distance between all the points is ' + result[num].points + ' : ' +  result[num].distance);
 }
 findTheShortest(0, A, B, C, D, E, F, G, H, I, J);
